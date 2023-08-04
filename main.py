fcbdec4416ce3e1155b979bf0a3cb5d76ae31af3
YI-CHIH-0126 <kingwang.tw2014@gmail.com>
2023-08-05 00:12:01 +0800

    change text

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask,request,render_template
 
 app=Flask(__name__)
@@ -18,7 +19,7 @@ def check():
         file=open('static/list.txt',mode='r')
         olist=file.readlines()
         html='''
-                <h1>order list</h1>
+                <h1>完成點餐</h1>
                 <table border>
             '''
         for i in olist:
@@ -28,7 +29,7 @@ def check():
         return html
     else:
         html='''
-                <h1>order list</h1>
+                <h1>完成點餐</h1>
                 <table border>
             '''
         file=open("static/list.txt","r")
