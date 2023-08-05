99ed55418223f3e433239c3a071ac3f96bb52527
YI-CHIH-0126 <kingwang.tw2014@gmail.com>
2023-08-06 03:30:16 +0800

    fix the path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask,request,render_template
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("./templates/home.html")
 
 @app.route("/order",methods=['POST','GET'])
 def order():
@@ -35,17 +35,17 @@ def order():
         f=open("./templates/order.html",'w',encoding="utf-8")
         f.write(html)
         f.close()
-        return render_template('order.html')
+        return render_template('./templates/order.html')
     else:
-        return render_template("order.html")
+        return render_template("./templates/order.html")
 
 @app.route("/check_order",methods=['POST','GET'])
 def check():
     if request.method =='POST':
-        file=open("static/list.txt",mode='a')
+        file=open("./static/list.txt",mode='a')
         print("{}:{}".format(request.form['phone'],request.form['meals']),file=file)
         file.close()
-        file=open('static/list.txt',mode='r')
+        file=open('./static/list.txt',mode='r')
         olist=file.readlines()
         html='''
                 <h1>完成點餐</h1>
@@ -61,7 +61,7 @@ def check():
                 <h1>完成點餐</h1>
                 <table border>
             '''
-        file=open("static/list.txt","r")
+        file=open("./static/list.txt","r")
         olist=file.readlines()
         for i in olist:
             html+="<tr><td>{}</td></tr>".format(i)
@@ -72,11 +72,11 @@ def check():
 def log_in():
     if request.method=="POST":
         if request.form['account']=="king" and request.form['pwd']=="0126":
-            return render_template("edit.html")
+            return render_template("./templates/edit.html")
         else:
             return "密碼錯誤"
     else:
-        return render_template("login.html")
+        return render_template("./templates/login.html")
     
 if __name__ == '__main__':
     app.run()
